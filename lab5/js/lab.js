82f80d66c0d433ca8dc66402155d11e07cc08816
MarcoFajardo <77114515+HunholySaiyan@users.noreply.github.com>
2021-01-27 20:13:48 -0800

    Lab 5

---
+++ lab5/js/lab.js
@@ -0,0 +1,22 @@
+/*
+  * Authors: Marco Fajardo,  <Maanfaja@ucsc.edu>
+  * Created: 27 January
+  * License: Public Domain
+*/
+
+//Variables
+
+  var make = "Ford";
+  var model = "Fusion";
+  var color = " Grey";
+  var year = 2010;
+  var onwIt = true;
+
+//Current Age
+    age = 2021 - year;
+
+      document.writeln("Make:" + make + "<br>");
+      document.writeln("Model: " + model + "<br>");
+      document.writeln("Color: " + color + "<br>");
+      document.writeln("Year: " + year + "<br>");
+      document.writeln("Age: " + age + "<br>");
