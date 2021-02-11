032f648befd61a7d91c8f56dc56337ac6c989508
MarcoFajardo <77114515+HunholySaiyan@users.noreply.github.com>
2021-02-10 18:12:51 -0800

    lab9

---
+++ SectionD/js/script2.js
@@ -0,0 +1,18 @@
+// author; Marco Fajardo
+// date: 2021/02/09
+//create a div element that has id="div_1" and text that says "this is annoying"
+//console.log that text
+//add a button
+//change the text from this is annoying into " this is more annoying than I thought!" when Clicking the button
+
+var divEl = document.getElementById("div_1");
+divEl.innerHTML = "this is annoying";
+console.log(divEl.innerHTML);
+
+var buttonEl = document.createElement("button");
+document.body.appendChild(buttonEl);
+buttonEl.innerHTML = "Press Me"
+
+buttonEl.onclick = function(){
+  divEl.innerHTML= "this is more annoying than I thought"
+}
---
+++ lab9/js/index.js
@@ -0,0 +1,18 @@
+/*
+  * Authors: Marco Fajardo, <Maanfaja@ucsc.edu>
+  * Created: 10 february 2021
+  * License: Public Domain
+*/
+
+  var outputEl = document.getElementById("output");
+
+
+  var new1El = document.createElement("h1");
+    new1El.innerHTML = "Today I am working on lab 9";
+      document.body.appendChild(new1El);
+      document.getElementById("output").style.fontStyle = "italic";
+
+
+  var new2El = document.createElement("h1");
+    new2El.innerHTML = "Hopefully I am doing this right";
+      document.body.appendChild(new2El);
