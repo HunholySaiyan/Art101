85eb246ed77d21fde4f7f37bcf3f769b4cc726bc
MarcoFajardo <77114515+HunholySaiyan@users.noreply.github.com>
2021-03-01 12:10:53 -0800

    Project

--- SectionD/js/script3.js
+++ SectionD/js/script3.js
@@ -9,11 +9,27 @@
 //
 //
 // }
-for (var index=0; index<10; index++){
-  if(index%2 == 0){
-    $("#container").append("<div id='box_id_" +(index+1)+" Class='box_even'></div>");
-  }else {
-    $("#container").append("<div id='box_id_" +(index+1)+" Class='box_odd'></div>");
-  }
+// for (var index=0; index<10; index++){
+//   if(index%2 == 0){
+//     $("#container").append("<div id='box_id_" +(index+1)+" Class='box_even'></div>");
+//   }else {
+//     $("#container").append("<div id='box_id_" +(index+1)+" Class='box_odd'></div>");
+//   }
+//
+// }
 
+var original = $("original").text();
+console.log(original);
+
+function matchingfunction(){
+  var input = $("input").val();
+  var substring_var = original.substring(0,input.length)
+  console.log(input);
+  console.log(substring_var)
+
+  if(input == substring_var){
+    $("#input").css('border-color', "blue");
+  } else { $("#input").css('border-color', 'red');
 }
+}
+$("#input").keyup(matchingfunction);
