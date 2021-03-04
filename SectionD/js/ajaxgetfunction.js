f174915b2f16131f07425abab9aef435eb45f491
MarcoFajardo <77114515+HunholySaiyan@users.noreply.github.com>
2021-03-03 19:48:30 -0800

    lab15

---
+++ SectionD/js/ajaxgetfunction.js
@@ -0,0 +1,25 @@
+function getajaxdata(){
+$.ajax({
+  url:"data/data.txt",
+  type:"GET",
+  datatype:"text",
+  success: successFun,
+  error: errorFun,
+  complete: function(xhr, status){
+    console.log("the request has been done");
+
+  }
+})
+}
+function successFun(result){
+  console.log(resutl)
+  // $('#ajax_text').append(result);
+  $('#ajax_text').html(result);
+}
+
+function errorFun(xhr, status, strErr){
+  console.log("error"+strErr);
+
+}
+
+$("ajax_get").click(getajaxdata)
---
+++ lab15/js/lab.js
@@ -0,0 +1,46 @@
+/*
+  * Authors: Marco Fajardo, <Maanfaja@ucsc.edu>
+  * Created: 3 March 2021
+  * License: Public Domain
+*/
+
+apiEndpoint = "https://api.thecatapi.com/v1/images/search";
+
+$("#activate").click(catApi);
+
+function textOnscreen(text){
+  $("#output").html(text);
+}
+
+function catApi (){
+$.ajax({
+    //my endpoint
+    url: "apiEndpoint",
+
+    data: {
+
+            id: 123,
+            api_key: "125c6ddb-369e-4f27-99cb-96b1c689e939",
+          },
+    //type would be GET
+    type: "GET",
+    // I need the json and image from the api
+    dataType : "json",
+
+    success: function(data) {
+      var textData = JSON.stringigy(data);
+      var str = ""
+      var imageURL = data.img;
+      // var title = data.title;
+      str += "<h2>" + title + "</h1>";
+      str += "<img src='" + imageURL + "'>"
+        console.log("Success", textData);
+        textOnscreen(str);
+    },
+
+    error: function (jqXHR, textStatus, errorThrown) {
+        console.log("Failure");
+        textOnscreen(errorThrown + " Status:" + status);
+    }
+})
+}
