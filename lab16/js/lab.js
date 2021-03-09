ec30363cd4774e27c1c194ade8b780b4d2d6979e
MarcoFajardo <77114515+HunholySaiyan@users.noreply.github.com>
2021-03-08 17:25:38 -0800

    lab16

--- lab16/js/lab.js
+++ lab16/js/lab.js
@@ -1,6 +1,6 @@
 /*
   * Authors: Marco Fajardo, <Maanfaja@ucsc.edu>
-  * Created: 3 March 2021
+  * Created: 8 March 2021
   * License: Public Domain
 */
 
@@ -15,12 +15,12 @@ function textOnscreen(text){
 function catApi (){
 $.ajax({
     //my endpoint
-    url: "https://api.thecatapi.com/v1/images/search",
+    url: "https://xkcd.com/666/info.0.json",
 
     data: {
 
             id: 123,
-            api_key: "125c6ddb-369e-4f27-99cb-96b1c689e939",
+            api_key: "",
           },
     //type would be GET
     type: "GET",
@@ -32,11 +32,10 @@ $.ajax({
       var str = "";
       var imageURL = data.img;
       var title = data.title;
-      var id = data.id;
-      var width = data.width;
-      var height = data.height;
-      var breed = data.breeds
-      var url = data.url;
+      var year = data.year;
+      var alt = data.alt;
+      var day = data.day;
+
       str += "<h2>" + title + "</h1>";
       str += "<img src='" + imageURL + "'>"
         console.log("Success", textData);
