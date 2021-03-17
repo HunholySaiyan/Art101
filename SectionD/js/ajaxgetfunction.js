ec6f5d8ac24e20246ceb251e82fde34bf14a37dd
MarcoFajardo <77114515+HunholySaiyan@users.noreply.github.com>
2021-03-16 18:58:25 -0700

    project

--- SectionD/js/ajaxgetfunction.js
+++ SectionD/js/ajaxgetfunction.js
@@ -1,25 +1,38 @@
 function getajaxdata(){
-$.ajax({
-  url:"data/data.txt",
-  type:"GET",
-  datatype:"text",
-  success: successFun,
-  error: errorFun,
-  complete: function(xhr, status){
-    console.log("the request has been done");
+// $.ajax({
+//   url:"https://en.wikipedia.org/api/rest_v1/page/summary/Warren_G._Harding?redirect=true",
+//   type:"GET",
+//   datatype:"json",
+//   success: successFun,
+//   error: errorFun,
+//   complete: function(xhr,status){
+//     console.log("the request has been done");
+//
+//   }
+// })
+// function successFun(result){
+//   console.log(result)
+//   //$('#ajax_text').append(result);
+//   $('#ajax_text').html(result.extract );
+// }
+// function errorFun(xhr,status,strErr){
+//   console.log("error "+strErr);
+// }
+const settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://genius.p.rapidapi.com/artists/16775/songs",
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-key": "77f9d9ff88msh9bc93d96fb25dacp1b7291jsn5be61e72cb6e",
+		"x-rapidapi-host": "genius.p.rapidapi.com"
+	}
+};
 
-  }
-})
+$.ajax(settings).done(function (response) {
+  $('#ajax_text').append("<img src="+response.response.songs[0].song_art_image_url+">");
+	console.log(response);
+});
 }
-function successFun(result){
-  console.log(resutl)
-  // $('#ajax_text').append(result);
-  $('#ajax_text').html(result);
-}
 
-function errorFun(xhr, status, strErr){
-  console.log("error"+strErr);
-
-}
-
-$("ajax_get").click(getajaxdata)
+$('#ajax_get').click(getajaxdata)
