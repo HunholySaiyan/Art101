db0428f18507b13533d2533640b7905ac4615228
MarcoFajardo <77114515+HunholySaiyan@users.noreply.github.com>
2021-03-16 22:38:25 -0700

    jonathan

--- Project0/js/project.js
+++ Project0/js/project.js
@@ -84,3 +84,19 @@ cardContainer3.addEventListener('click', e => {
     cardLessBtn3.parentNode.parentNode.classList.remove('card--open3')
   }
 })
+
+const cardContainer4 = document.querySelector('.cards4')
+
+
+cardContainer4.addEventListener('click', e => {
+  const cardInfoBtn4 = e.target.closest('.card__more-info4')
+  const cardLessBtn4 = e.target.closest('.card__less-info4')
+
+  if (cardInfoBtn4) {
+    cardInfoBtn4.parentNode.parentNode.classList.add('card--open4')
+  }
+
+  if (cardLessBtn4) {
+    cardLessBtn4.parentNode.parentNode.classList.remove('card--open4')
+  }
+})
